2ee78959f3ce108a21ebbddb01b09c75990a6336
kolp143 <92522193+kolp143@users.noreply.github.com>
2023-03-23 12:20:12 +0800

    try to push

--- app/src/main/java/com/example/iseeyou/GetStarted/GetStarted1.kt
+++ app/src/main/java/com/example/iseeyou/GetStarted/GetStarted1.kt
@@ -1,16 +1,16 @@
 package com.example.iseeyou.GetStarted
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.iseeyou.R
 
 class GetStarted1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.getstarted1)
-
+        //testing
         var button1 = findViewById<Button>(R.id.button)
         button1.setOnClickListener{
             val intent1 = Intent(this, GetStarted2::class.java)
